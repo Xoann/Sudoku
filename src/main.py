d55baae04177848c54f6d5a043f7cc0cc98300a3
Liam Maguire <liammaguire@outlook.com>
2023-12-22 12:44:21 -0500

    Setup validator function

--- src/main.py
+++ src/main.py
@@ -1,6 +1,32 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+from flask import Flask, request, jsonify
+
+from src.sudoku import Sudoku
+from src.sudoku_solver import SudokuSolver
+
+app = Flask(__name__)
+
+
+def backtracking_solve(data: list[list[int]]) -> list[list[int]]:
+    sudoku = Sudoku(board=data)
+    solver = SudokuSolver(sudoku)
+    solver.solve_backtrack()
+    return sudoku.get_board()
+
+
+@app.route('/validate', methods=['POST'])
+def validate():
+    try:
+        data = request.get_json()
+        if not data or not isinstance(data, list):
+            return jsonify({'error': 'Invalid data format'}), 400
+
+        sudoku = Sudoku(board=data)
+        result = sudoku.is_valid()
+        return jsonify({'result': result})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run(debug=True)
---
+++ src/sudoku_generator.py
@@ -0,0 +1,13 @@
+import random
+from typing import Optional
+from sudoku import Sudoku
+
+
+def generate_completed_sudoku(size: int = 3) -> Sudoku:
+    """First implement solver
+
+    :param size: 
+    :return:
+    """
+    return None
+
